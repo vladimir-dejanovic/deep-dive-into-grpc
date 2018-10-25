4c4733e4cb92b189063cefbb1c6cf756d4c48504
Vladimir Dejanovic <vladimir@itshark.xyz>
2018-10-25 06:45:25 +0200

    init version after running  mvn archetype:generate

---
+++ src/main/java/xyz/itshark/conf/talk/App.java
@@ -0,0 +1,13 @@
+package xyz.itshark.conf.talk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
